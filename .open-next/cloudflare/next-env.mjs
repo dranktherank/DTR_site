d7e07c61d38ff74d1aca97b2c9eed47272c088df
wtcarrin <wtcarrin@go.olemiss.edu>
2025-07-15 23:16:09 -0500

    Reset!

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://zwaxhzmuzxcptlhifdjt.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3YXhoem11enhjcHRsaGlmZGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzODkzMTIsImV4cCI6MjA2NTk2NTMxMn0.ERPqEbPbghbyFgv6hzI5J1_FpDYBYnMOgTVITwwgdZ0"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://zwaxhzmuzxcptlhifdjt.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3YXhoem11enhjcHRsaGlmZGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzODkzMTIsImV4cCI6MjA2NTk2NTMxMn0.ERPqEbPbghbyFgv6hzI5J1_FpDYBYnMOgTVITwwgdZ0"};
+export const test = {};
